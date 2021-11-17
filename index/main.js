d6ed9d9c39bd45ba9df7774dc3b6f7f60c1c222c
unknown <monserrat_1320114183@uptecamac.edu.mx>
2021-11-17 15:23:58 -0600

    unidad2

---
+++ index/main.js
@@ -0,0 +1,17 @@
+var elemento1 = [11,2,3,45,5,64,78,9,70];
+var elemento2 = [10,51,41,8,71,81,10,2,1];
+
+console.log(elemento1);
+console.log(elemento2);
+
+console.log("-------------------------------");
+for (var i = 0; i < elemento1.length; i++) {
+  //elemento2.push(elemento1[i]);
+  if(elemento1 = elemento2)
+  {
+      console.log("elementos repetidos: " + i )
+  }
+}
+
+console.log(elemento1);
+console.log(elemento2);
